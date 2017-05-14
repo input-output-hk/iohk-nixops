c9ab388ae89663bd90a62dccea91410d0924adab
Kristijan Šarić <kristijanaric@yahoo.com>
2017-05-14 21:16:32 +0200

    Update deploy-explorer-linux.sh
    Fixed to relative path.

--- scripts/deploy-explorer-linux.sh
+++ scripts/deploy-explorer-linux.sh
@@ -6,15 +6,15 @@ if [ -z "$HASH_COMMIT" ]
     exit 1
 fi
 
-cd ~/devops-86-deploy-staging-explorer/pkgs
+cd ../pkgs
 # MAC users, beware! Not sure if the flags are correct.
 sed -ri "s/cabal2nix https:\/\/github\.com\/input-output-hk\/cardano-sl-explorer\.git --no-check --revision(.*?)> cardano-sl-explorer.nix/cabal2nix https:\/\/github\.com\/input-output-hk\/cardano-sl-explorer\.git --no-check --revision $HASH_COMMIT > cardano-sl-explorer.nix/g" generate.sh
 ./generate.sh 
-cd ~/devops-86-deploy-staging-explorer/cardano-sl-explorer/frontend
+cd ../cardano-sl-explorer/frontend
 git stash
 git fetch
 git checkout $HASH_COMMIT
 git stash pop
 NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/ebbababd8f9cb49d039f11d58e4e49d8e02d7533.tar.gz EXPLORER_NIX_FILE=./../default.nix ./scripts/build.sh
-cd ~/devops-86-deploy-staging-explorer/
+cd ..
 ./CardanoCSL.hs deploy -c config.yaml
