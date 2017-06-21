b799a5e0d467e8d912cef5898b45fdb48bb101cc
michael bishop <michael.bishop@iohk.io>
2017-06-21 10:36:47 -0300

    testing hydra github status

--- tests/simple-node.nix
+++ tests/simple-node.nix
@@ -12,6 +12,7 @@ import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ... }: {
   testScript = ''
     startAll
     $machine->waitForUnit("cardano-node.service");
+
     # TODO, implement sd_notify?
     $machine->waitForOpenPort(3000);
   '';
