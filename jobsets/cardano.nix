62c754e682c313e9459d0d25d380c75f8811cf4b
michael bishop <michael.bishop@iohk.io>
2017-06-23 15:20:56 -0300

    also lock in the nixpkgs used for jobset eval PR #78 only covered the default.nix that generates jobsets

--- jobsets/cardano.nix
+++ jobsets/cardano.nix
@@ -1,9 +1,11 @@
-{ pkgs ? (import <nixpkgs> {}), supportedSystems ? [ "x86_64-linux" ] }:
+let
+  fixedNixpkgs = (import <nixpkgs> {}).fetchFromGitHub (builtins.fromJSON (builtins.readFile ../nixpkgs-src.json));
+in { pkgs ? (import fixedNixpkgs {}), supportedSystems ? [ "x86_64-linux" ] }:
 
 with pkgs;
 
 let
-  iohkpkgs = import ./../default.nix {};
+  iohkpkgs = import ./../default.nix { inherit pkgs; };
 in rec {
   inherit (iohkpkgs) cardano-report-server-static cardano-sl-static cardano-sl-explorer-static cardano-sl;
   stack2nix = iohkpkgs.callPackage ./../pkgs/stack2nix.nix {};
